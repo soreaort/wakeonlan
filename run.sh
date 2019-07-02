ad345ecf6992bfbcc7bb15f848960ab1c337eed4
Saul Orea <soreaort@gmail.com>
2019-07-01 23:56:03 -0500

    Add run.sh
    Signed-off-by: Saul Orea <soreaort@gmail.com>

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROVISIONING_PATH="/usr/lib/python3/dist-packages/provisioningserver/"
+
+echo "Copying wakeonlan.py..."
+cp -vf wakeonlan.py ${PROVISIONING_PATH}drivers/power/wakeonlan.py
+
+echo "Import WakeOnLANPowerDriver..."
+grep -qxF "from provisioningserver.drivers.power.wakeonlan import WakeOnLANPowerDriver" ${PROVISIONING_PATH}drivers/power/registry.py || sed '/from provisioningserver.utils.registry import Registry/i from provisioningserver.drivers.power.wakeonlan import WakeOnLANPowerDriver' -i ${PROVISIONING_PATH}drivers/power/registry.py
+
+echo "Adding WakeOnLANPowerDriver() method..."
+grep -qxF "    WakeOnLANPowerDriver()," ${PROVISIONING_PATH}drivers/power/registry.py || sudo sed '/power_drivers = \[/a\    WakeOnLANPowerDriver(),' -i ${PROVISIONING_PATH}drivers/power/registry.py
+
+
+echo "Restarting rackd and regiond..."
+systemctl restart maas-rackd.service maas-regiond.service
